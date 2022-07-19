590e78f35e5beb42f7ccea5e54ef2ed6e748f350
Jemaul Hunter <jemaulhunter@gmail.com>
2022-07-19 10:18:03 -0400

    commit tree afb05dfe6a3d6b46c1454e916386238b65ab8abb

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceTravelledInfFeet(start, destination) {
+    return Math.abs(start - destination) * 264
+}
+function distanceTravelledInfFeet(start, destination) {
+    return Math.abs(start - destination) * 264
+}
+
+const distanceTravelledInFeet = (start, destination) => Math.abs(start - destination)
+
+// const functionName = function(param, param) {
+    //Stuff
+//}
+function calculatesFarepreice(start, destination) {
+    const traveled = distanceTravelledInFeet(start, destination);
+    //travel less than 400 so, 0-399
+    if(traveled < 400) {
+        //traveled between 400 2000
+    } else if (traveled < 2000) {
+        //traveled >= 400 && traveled < 2000
+        return (traveled - 400) * .02
+        //traveled between: 2000 to 2500
+   } else if (traveled <= 2500){
+    return 25
+   }else {
+    return 'cannot travel that far'
+   }
+}
+  
